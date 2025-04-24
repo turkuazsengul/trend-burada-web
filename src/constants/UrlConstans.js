b75b6940f0a150b725bb5f0e604ad362c0c2ac37
turkuazsengul <turkuazsengul@gmail.com>
2025-04-25 00:17:09 +0300

    env base system created.

--- src/constants/UrlConstans.js
+++ src/constants/UrlConstans.js
@@ -1,16 +1,16 @@
-export const KEYCLOAK_BASE_URL = "http://localhost:8180"
-export const KEYCLOAK_TOKEN_URL = "http://localhost:8180/auth/realms/TrendBurada/protocol/openid-connect/token"
+export const KEYCLOAK_BASE_URL = process.env.REACT_APP_KEYCLOAK_BASE_URL
+export const KEYCLOAK_TOKEN_URL = process.env.REACT_APP_KEYCLOAK_TOKEN_URL
 
 
-export const LOGIN_URL = "http://localhost:40000/api/v1/user/auth/login"
-export const USER_INFO_URL = "http://localhost:40000/api/v1/user/management/get"
-export const USER_UPDATE_URL = "http://localhost:40000/api/v1/user/management/update"
-export const REGISTER_URL = "http://localhost:40000/api/v1/user/auth/register"
-export const LOGOUT_URL = "http://localhost:40000/api/v1/auth/logout"
+export const LOGIN_URL = process.env.REACT_APP_LOGIN_URL
+export const USER_INFO_URL = process.env.REACT_APP_USER_INFO_URL
+export const USER_UPDATE_URL = process.env.REACT_APP_USER_UPDATE_URL
+export const REGISTER_URL = process.env.REACT_APP_REGISTER_URL
+export const LOGOUT_URL = process.env.REACT_APP_LOGOUT_URL
 
-export const KEYCLOAK_CLIENT_ID = "login-app"
-export const KEYCLOAK_GRANT_TYPE = "password"
+export const KEYCLOAK_CLIENT_ID = process.env.REACT_APP_KEYCLOAK_CLIENT_ID
+export const KEYCLOAK_GRANT_TYPE = process.env.REACT_APP_KEYCLOAK_GRANT_TYPE
 
 
-export const MY_USER_INFO_URL = "/hesabım/KullaniciBilgilerim"
-export const MY_ORDER_URL = "/hesabım/Siparişlerim"
+export const MY_USER_INFO_URL = process.env.REACT_APP_MY_USER_INFO_URL
+export const MY_ORDER_URL = process.env.REACT_APP_MY_ORDER_URL
